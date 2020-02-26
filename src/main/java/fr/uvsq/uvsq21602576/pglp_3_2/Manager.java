04ff6cf9d00ac54eb0dbd72fc2207f377c83bd7d
Flora Carré <flora.carre@ens.uvsq.fr>
2020-02-26 12:22:27 +0100

    Ajout classe Manager et calcul salaire, et test unitaire correspondant

---
+++ src/main/java/fr/uvsq/uvsq21602576/pglp_3_2/Manager.java
@@ -0,0 +1,16 @@
+package fr.uvsq.uvsq21602576.pglp_3_2;
+
+public class Manager extends Employe {
+	private int nbPersonnel;
+
+	public Manager(String nom, int dateArrivee, int nbPersonnel) {
+		super(nom, dateArrivee);
+		this.nbPersonnel=nbPersonnel;
+	}
+	
+	@Override
+	public double calculSalaire() {
+		return super.calculSalaire()+100*this.nbPersonnel;
+	}
+	
+}
---
+++ src/test/java/fr/uvsq/uvsq21602576/pglp_3_2/ManagerTest.java
@@ -0,0 +1,17 @@
+package fr.uvsq.uvsq21602576.pglp_3_2;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.Calendar;
+
+import org.junit.Test;
+
+public class ManagerTest {
+
+	@Test
+	public void calculSalaireTest() {
+		Manager M = new Manager("Prenom Nom", 2015, 3);
+		assertEquals(1500+(Calendar.getInstance().get(Calendar.YEAR)-2015)*20+100*3,M.calculSalaire(),0);
+	}
+
+}
