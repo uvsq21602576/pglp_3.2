afa3e8d9ee6703997fa12dc4251eac9cc16d47e5
Flora Carré <flora.carre@ens.uvsq.fr>
2020-03-21 17:24:38 +0100

    Employé modifié pour être sans erreur checkstyle

--- src/main/java/fr/uvsq/uvsq21602576/pglp_3_2/Employe.java
+++ src/main/java/fr/uvsq/uvsq21602576/pglp_3_2/Employe.java
@@ -2,19 +2,49 @@ package fr.uvsq.uvsq21602576.pglp_3_2;
 
 import java.util.Calendar;
 
+/**
+ * Représentante un employé.
+ * Il possède un nom et une année d'arrivée dans l'entreprise.
+ * @author Flora
+ */
 public class Employe {
-	private final String nom;
-	private final int dateArrivee;
-	
-	public Employe(String nom, int dateArrivee) {
-		this.nom = nom;
-		this.dateArrivee = dateArrivee;
-	}
-	
-	public double calculSalaire() {
-		int anciennete = Calendar.getInstance().get(Calendar.YEAR) - this.dateArrivee;
-		return 1500 + 20 * anciennete;
-	}
+    /**
+     * Nom de l'employé.
+     */
+    private final String nom;
+    /**
+     * Année d'arrivée dans l'entreprise.
+     */
+    private final int dateArrivee;
 
+    /**
+     * Salaire de base.
+     */
+    private static final int BASE_SALAIRE = 1500;
+    /**
+     * Salaire ajouté par année d'ancienneté.
+     */
+    private static final int SALAIRE_PAR_ANNEE = 20;
+
+    /**
+     * Constructeur.
+     * Crée un employé avec un nom et son année d'arrivée dans l'entreprise.
+     * @param n   Nom de l'employé
+     * @param anneeArrivee   Date de son arrivée
+     */
+    public Employe(final String n, final int anneeArrivee) {
+        this.nom = n;
+        this.dateArrivee = anneeArrivee;
+    }
+
+    /**
+     * Retourne le salaire de l'employé.
+     * Calculé en fonction de l'année d'arrivée, donc de l'ancienneté.
+     * @return  salaire
+     */
+    public double calculSalaire() {
+        int date = Calendar.getInstance().get(Calendar.YEAR);
+        int anciennete = date - this.dateArrivee;
+        return BASE_SALAIRE + SALAIRE_PAR_ANNEE * anciennete;
+    }
 }
-
---
+++ src/main/java/fr/uvsq/uvsq21602576/pglp_3_2/package-info.java
@@ -0,0 +1,6 @@
+
+/**
+ * Contient le code du programme : les différents employés et l'entreprise.
+ */
+package fr.uvsq.uvsq21602576.pglp_3_2;
+
